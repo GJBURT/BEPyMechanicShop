3519599e42329f2598cebf431d6340bea550c981
Geoffrey Burt <geoffreyjburt@gmail.com>
2025-05-28 02:42:49 -0400

    Commented out several print statements that were used for debugging.

--- flask_app.py
+++ flask_app.py
@@ -13,15 +13,15 @@ import secrets
 # print(secrets.token_hex(16))  # Print a random secret key for debugging
 
 # Print to debug the loading of the .env variables
-print(f"DB_USER: {os.getenv('DB_USER')}, DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
+# print(f"DB_USER: {os.getenv('DB_USER')}, DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
 
 # Determine config name
 config_name = os.getenv('FLASK_ENV', 'development')
 # config_class = config_by_name.get(config_name)
 
-print(f'Using configuration: {config_name}')
-print(f"DB_USER: {os.getenv('DB_USER')}")
-print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
+# print(f'Using configuration: {config_name}')
+# print(f"DB_USER: {os.getenv('DB_USER')}")
+# print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
 
 app = create_app(config_name)
 
